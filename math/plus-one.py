b0433e0446273bd2887e5e1363a107efcc000296
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-01-18 17:34:54 +1100

    LC-plus-one [Runtime: 0 ms; Memory: 19.6 MB]

--- math/plus-one.py
+++ math/plus-one.py
@@ -1,7 +1,23 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = 0
-        for i in range(len(digits)):
-            num = num * 10 + digits[i]
-        num = num + 1
-        return [int(d) for d in str(num)]
+        if digits[len(digits) - 1] != 9:
+            return digits[:(len(digits) - 1)] + [digits[len(digits) - 1] + 1]
+        
+        # when last digit == 9
+        i = len(digits) - 1
+        if len(digits) == 1:
+            return [int(d) for d in str(10)]
+        
+        return self.plusOne(digits[:(len(digits) - 1)]) + [0]
+
+        
+        
+        
+        
+        
+        
+        # num = 0
+        # for i in range(len(digits)):
+        #     num = num * 10 + digits[i]
+        # num = num + 1
+        # return [int(d) for d in str(num)]

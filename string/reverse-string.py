200965f4a3e73aa6faec88b36d20f5947363c50b
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-01-25 21:38:14 +1100

    LC-reverse-string [Runtime: 0 ms; Memory: 23.6 MB]

--- string/reverse-string.py
+++ string/reverse-string.py
@@ -3,9 +3,6 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        for i in range(len(s)//2):
-            temp = s[i]
-            s[i] = s[len(s)-i-1]
-            s[len(s)-i-1] = temp
+        s[:] = s[::-1]
             
         

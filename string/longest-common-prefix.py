365ed6ad43e445627267bb626707eaa0271e9755
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-01-26 16:24:56 +1100

    LC-longest-common-prefix [Runtime: 0 ms; Memory: 19.3 MB]

--- string/longest-common-prefix.py
+++ string/longest-common-prefix.py
@@ -1,24 +1,10 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 1:
-            return strs[0]
-        
-        commonPrefix = strs[0]
 
-        for string in strs[1:]:
-            commonPrefix = self.twoCommonPrefix(commonPrefix, string)
-            if commonPrefix == "":
-                return commonPrefix
-        
-        return commonPrefix
-        
-        
-    def twoCommonPrefix(self, first_str:str, second_str:str) -> str:
-        commonPrefix = ""
-        for i in range(min(len(first_str), len(second_str))):
-            if first_str[i] != second_str[i]:
-                break
-            # if first_str[i] == second_str[i]
-            commonPrefix += first_str[i]
-        
-        return commonPrefix
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != char:
+                    return strs[0][:i]
+            
+        return strs[0]

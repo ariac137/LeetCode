d358f0eed1251d54b6efad0bf52639c5ee479248
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-01-26 13:59:14 +1100

    LC-valid-anagram [Runtime: 11 ms; Memory: 19.5 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,15 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        SIZE = 26
-        count = [0 for _ in range(SIZE)]
+        if len(s) != len(t):
+            return False
+            
+        count = [0] * 26  # Shorthand for initialization
+        ord_a = ord('a')  # Cache this value so you don't call it in the loop
+        
         for char in s:
-            index = ord(char) - ord('a')
-            count[index] += 1
+            count[ord(char) - ord_a] += 1
             
         for char in t:
-            index = ord(char) - ord('a')
-            count[index] -= 1
-            if count[index] < 0:
+            idx = ord(char) - ord_a
+            count[idx] -= 1
+            if count[idx] < 0:
                 return False
         
-        return not any(count)
+        return True # No need for any() if lengths were equal!

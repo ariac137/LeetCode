da4e12c4ce4f90cb244a1633900e5ed45f805041
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-01-26 14:35:58 +1100

    LC-valid-palindrome [Runtime: 12 ms; Memory: 23.6 MB]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -1,13 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new_s = [i.lower() for i in s if i.isalnum()]
-        i = 0
-        j = len(new_s) - 1
-        while i < j:
-
-            if new_s[i] != new_s[j]:
-                return False
-            i += 1
-            j -= 1
-            
-        return True
+        # Filter in one pass
+        filtered = "".join(char.lower() for char in s if char.isalnum())
+        # Compare to reverse
+        return filtered == filtered[::-1]

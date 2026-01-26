f5893c3d56abcd7752aef40652b329e41b516ada
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-01-26 11:39:22 +1100

    LC-first-unique-character-in-a-string [Runtime: 42 ms; Memory: 19.7 MB]

--- string/first-unique-character-in-a-string.py
+++ string/first-unique-character-in-a-string.py
@@ -1,11 +1,15 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        enum_s = list(enumerate(s))
-        while enum_s:
-            temp = enum_s.pop(0)
-            temp_index, temp_s = temp
-            length = len(enum_s)
-            enum_s = [(i, item) for i, item in enum_s if item != temp_s]
-            if len(enum_s) == length:
-                return temp_index
+        duplicated_items = []
+        for i in range(len(s)):
+            item = s[i]
+            if item in duplicated_items:
+                continue
+            if i >= len(s) - 1:
+                return i
+            if item in s[i + 1:]:
+                duplicated_items.append(item)
+            else: 
+                return i
+            
         return -1

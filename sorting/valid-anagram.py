5b91d03ff0fd2a2d377aec506f2290ce65353963
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-01-26 14:04:52 +1100

    LC-valid-anagram [Runtime: 11 ms; Memory: 19.5 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,18 +1,16 @@
+# Unicode-safe version
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
             
-        count = [0] * 26  # Shorthand for initialization
-        ord_a = ord('a')  # Cache this value so you don't call it in the loop
-        
+        counts = {}
         for char in s:
-            count[ord(char) - ord_a] += 1
-            
-        for char in t:
-            idx = ord(char) - ord_a
-            count[idx] -= 1
-            if count[idx] < 0:
-                return False
+            counts[char] = counts.get(char, 0) + 1
         
-        return True # No need for any() if lengths were equal!
+        for char in t:
+            if char not in counts or counts[char] == 0:
+                return False
+            counts[char] -= 1
+            
+        return True

fe6863bec5a45501f74791a555b4bfa1e4303804
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-02-01 10:17:43 +1100

    LC-delete-node-in-a-linked-list [Runtime: 43 ms; Memory: 19.7 MB]

--- linked-list/delete-node-in-a-linked-list.py
+++ linked-list/delete-node-in-a-linked-list.py
@@ -10,8 +10,6 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        nextNode = node.next
-        node.val = nextNode.val
-        temp = nextNode.next
-        node.next = temp
-        nextNode.next = None
+        
+        node.val = node.next.val
+        node.next = node.next.next

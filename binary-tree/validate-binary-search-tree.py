f02362dad47f93e6a33ac18eb1a71809cd35151b
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-02-03 09:55:07 +1100

    LC-validate-binary-search-tree [Runtime: 252 ms; Memory: 21 MB]

--- binary-tree/validate-binary-search-tree.py
+++ binary-tree/validate-binary-search-tree.py
@@ -12,12 +12,28 @@ class Solution:
         if not root.left and not root.right:
             return True
         
-        if root.left and root.left.val >= root.val:
+        if root.left and max(self.treeToList(root.left)) >= root.val:
             return False
         
-        if root.right and root.right.val <= root.val:
+        if root.right and min(self.treeToList(root.right)) <= root.val:
             return False
         
         return self.isValidBST(root.left) and self.isValidBST(root.right)
+    
+
+    def treeToList(self, root: Optional[TreeNode]) -> list:
+        if not root:
+            return []
+        left = right = []
+        if root.left:
+            left = self.treeToList(root.left)
+        if root.right:
+            right = self.treeToList(root.right)
+
+        return left + [root.val] + right
+        
+        
+                
+                
 
                 

46b6fe4f63f617ab278ca0283d657bcab119666d
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-02-28 05:41:00 +1100

    Time: 0 ms (100%), Space: 19.5 MB (10.05%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,30 +1,6 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        x_bit = self.generateRevBits(x)
-        y_bit = self.generateRevBits(y)
-        print(x_bit, y_bit)
-        
-        count = 0
-        
-        extend = (abs(len(x_bit) - len(y_bit))) * "0"
-        
-        if len(x_bit) < len(y_bit):
-            x_bit += extend
-        else:
-            y_bit += extend
-            
-        for i in range(len(x_bit)):
-            if x_bit[i] != y_bit[i]:
-                count += 1
-            
-        
-        return count
-        
-        
-        
-    def generateRevBits(self, x: int) -> str:
-        if x == 0:
-            return "0"
+        return (x ^ y).bit_count()
         
         if x == 1:
             return "1"

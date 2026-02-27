f6638ae3539b3223eed81c7dddc32d95295189be
Aria Huang <120767288+ariac137@users.noreply.github.com>
2026-02-28 05:41:09 +1100

    Time: 0 ms (100%), Space: 19.4 MB (31.42%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,15 +1,4 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         return (x ^ y).bit_count()
-        
-        if x == 1:
-            return "1"
-        
-        result = ""
-        
-        while x != 1:
-            bit = x % 2
-            result = result + str(bit)
-            x = x // 2
-            
-        return result + "1"
+    
